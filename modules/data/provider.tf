7715fa75b9613ffe4adf74a72f62ce30b7f507fd
Massimo Manfredino <massimo.manfredino@gmail.com>
2024-10-17 15:52:45 +0200

    Refactors authentication

--- modules/data/provider.tf
+++ modules/data/provider.tf
@@ -8,6 +8,16 @@ terraform {
 }
 
 # Authentication
+variable "DATABRICKS_HOST" {
+  description   = "The Databricks workspace to interact with"
+  type          = string
+}
+
+variable "DATABRICKS_TOKEN" {
+  description   = "The Databricks workspace token to use"
+  type          = string
+}
+
 provider "databricks" {
   host  = var.DATABRICKS_HOST
   token = var.DATABRICKS_TOKEN
--- modules/data/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "DATABRICKS_HOST" {
-  description   = "The Databricks workspace to interact with"
-  type          = string
-}
-
-variable "DATABRICKS_TOKEN" {
-  description   = "The Databricks workspace token to use"
-  type          = string
-}
