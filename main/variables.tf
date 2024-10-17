1d3b57828d7840853ba0d6223542fb8931a9d707
Massimo Manfredino <massimo.manfredino@gmail.com>
2024-10-17 15:22:22 +0200

    Adds environment variables

---
+++ main/variables.tf
@@ -0,0 +1,9 @@
+variable "DATABRICKS_HOST" {
+  description   = "The Databricks workspace to interact with"
+  type          = string
+}
+
+variable "DATABRICKS_TOKEN" {
+  description   = "The Databricks workspace token to use"
+  type          = string
+}
